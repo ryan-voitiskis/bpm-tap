556861a66a33a8c5bf6d4ea3718b9ddd388a5766
ryan voitiskis <ryanvoitiskis@pm.me>
2024-04-20 14:17:45 +1000

    feat: #tap_button active styling

--- src/App.vue
+++ src/App.vue
@@ -91,6 +91,19 @@ const currentBpm = computed(() => (state.showBpm ? state.bpm.toFixed(1) : ""))
   appearance: none;
   background-color: v-bind(backgroundColour);
   transition: background-color 100ms linear;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 3;
+    transition: background-color 70ms linear;
+  }
+  &:active::before {
+    background-color: hsla(0, 0%, 100%, 0.15);
+  }
 }
 
 #tap_prompt {
